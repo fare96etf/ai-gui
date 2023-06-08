58c960bb117953b4a960056c566ae91acf444b26
Faris Kantic <faris_kantic@hotmail.com>
2023-06-08 23:45:25 +0200

    small fix predictions

--- ai_project/neural_nets_app/nn_scripts/nn_scripts.py
+++ ai_project/neural_nets_app/nn_scripts/nn_scripts.py
@@ -27,7 +27,7 @@ def run_neural_network(data, layers, outputs, compile_params, fit_params):
     model = Sequential()
     
     first_layer = layers[0]
-    model.add(Dense(first_layer["neurons"], input_shape=(col_no-len(outputs)), activation=first_layer["activation"]))
+    model.add(Dense(first_layer["neurons"], input_shape=(col_no-len(outputs),), activation=first_layer["activation"]))
     
     for layer in layers[1:]:
         model.add(Dense(layer["neurons"], activation=layer["activation"]))
@@ -37,8 +37,13 @@ def run_neural_network(data, layers, outputs, compile_params, fit_params):
     # fit the keras model on the dataset
     model.fit(X, y, epochs=fit_params["epochs"], batch_size=fit_params["batch_size"], verbose=0)
     # make class predictions with the model
-    predictions = (model.predict(X) > 0.5).astype(int)
+    predictions = (model.predict(X))
     # summarize the first 5 cases
     for i in range(5):
-        print('%s => %d (expected %d)' % (X[i], predictions[i], y[i][0]))
+        print(X[i], end="")
+        print(" => ", end="")
+        print(predictions[i], end="")
+        print(" (expected ", end="")
+        print(y[i], end="")
+        print(")")
     print("TRAINING COMPLETED")
